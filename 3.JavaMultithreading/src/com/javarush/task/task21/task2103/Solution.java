55141bff683a3460b48c30023083c20d67dda08d
Boris.Osechinskiy <boris.osechinskiy@MacBook-Pro-X5-3.local>
2022-08-08 23:40:51 +0300

    Выполнил задание task2103

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2103/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2103/Solution.java
@@ -6,7 +6,7 @@ package com.javarush.task.task21.task2103;
 
 public class Solution {
     public static boolean calculate(boolean a, boolean b, boolean c, boolean d) {
-        return (a && b && c && !d) || (!a && c) || (!b && c) || (c && d);
+        return c;
     }
 
     public static void main(String[] args) {
