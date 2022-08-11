fbb6a873f429dbc3907db38466fe118ce73ddc33
Boris.Osechinskiy <boris.osechinskiy@MacBook-Pro-X5-3.local>
2022-08-11 22:12:49 +0300

    Выполнил задачу 2113.6

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task21.task2113;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Hippodrome {
@@ -10,11 +11,18 @@ public class Hippodrome {
     }
 
     public List<Horse> getHorses() {
+
         return horses;
     }
 
-    public static void main(String[] args) {
+    static Hippodrome game;
 
+    public static void main(String[] args) {
+        ArrayList<Horse> horses = new ArrayList<>();
+        horses.add(new Horse("Риша", 3, 0));
+        horses.add(new Horse("Мышь", 3, 0));
+        horses.add(new Horse("Гиря", 3, 0));
+        game = new Hippodrome(horses);
     }
 }
 
