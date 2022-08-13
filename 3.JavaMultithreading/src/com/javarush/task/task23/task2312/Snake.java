3952ab83f8c8ccd20353f2d71fef2f8653e034d3
Boris Osechinskiy <osechinskiy@gmail.com>
2022-08-13 23:23:34 +0300

    Task 2312.18

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
@@ -3,15 +3,25 @@ package com.javarush.task.task23.task2312;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Класс змея
+ */
 public class Snake {
-    private List<SnakeSection> sections;
-    private boolean isAlive;
+    // Направление движения змеи
     private SnakeDirection direction;
+    // Состояние - жива змея или нет.
+    private boolean isAlive;
+    // Список кусочков змеи.
+    private ArrayList<SnakeSection> sections;
 
     public Snake(int x, int y) {
         sections = new ArrayList<SnakeSection>();
         sections.add(new SnakeSection(x, y));
         isAlive = true;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
     }
 
     public int getX() {
@@ -22,21 +32,16 @@ public class Snake {
         return sections.get(0).getY();
     }
 
+    public SnakeDirection getDirection() {
+        return direction;
+    }
+
     public void setDirection(SnakeDirection direction) {
         this.direction = direction;
     }
 
-    public List<SnakeSection> getSections() {
-
+    public ArrayList<SnakeSection> getSections() {
         return sections;
-    }
-
-    public boolean isAlive() {
-        return isAlive;
-    }
-
-    public SnakeDirection getDirection() {
-        return direction;
     }
 
     /**
@@ -60,7 +65,47 @@ public class Snake {
      * Метод перемещает змею в соседнюю клетку.
      * Координаты клетки заданы относительно текущей головы с помощью переменных (dx, dy).
      */
-    void move(int dx, int dy) {
+    private void move(int dx, int dy) {
+        // Создаем новую голову - новый "кусочек змеи".
+        SnakeSection head = sections.get(0);
+        head = new SnakeSection(head.getX() + dx, head.getY() + dy);
 
+        // Проверяем - не вылезла ли голова за границу комнаты
+        checkBorders(head);
+        if (!isAlive) return;
+
+        // Проверяем - не пересекает ли змея  саму себя
+        checkBody(head);
+        if (!isAlive) return;
+
+        // Проверяем - не съела ли змея мышь.
+        Mouse mouse = Room.game.getMouse();
+        if (head.getX() == mouse.getX() && head.getY() == mouse.getY()) // съела
+        {
+            sections.add(0, head);                  // Добавили новую голову
+            Room.game.eatMouse();                   // Хвост не удаляем, но создаем новую мышь.
+        } else // просто движется
+        {
+            sections.add(0, head);                  // добавили новую голову
+            sections.remove(sections.size() - 1);   // удалили последний элемент с хвоста
+        }
+    }
+
+    /**
+     * Метод проверяет - находится ли новая голова в пределах комнаты
+     */
+    private void checkBorders(SnakeSection head) {
+        if ((head.getX() < 0 || head.getX() >= Room.game.getWidth()) || head.getY() < 0 || head.getY() >= Room.game.getHeight()) {
+            isAlive = false;
+        }
+    }
+
+    /**
+     * Метод проверяет - не совпадает ли голова с каким-нибудь участком тела змеи.
+     */
+    private void checkBody(SnakeSection head) {
+        if (sections.contains(head)) {
+            isAlive = false;
+        }
     }
 }
