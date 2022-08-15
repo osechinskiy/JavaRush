6ba87e1645bcb6b3c69219856232b6391c400c4e
Boris Osechinskiy <osechinskiy@gmail.com>
2022-08-15 13:00:58 +0300

    Task 2413.15

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
@@ -1,17 +1,62 @@
 package com.javarush.task.task24.task2413;
 
 public class Ball extends BaseObject{
-    public Ball(double x, double y, double radius) {
-        super(x, y, radius);
+    private double speed;
+    private double direction;
+    private double dx;
+    private double dy;
+    private boolean isFrozen;
+
+    public Ball(double x, double y, double speed, double direction) {
+        super(x, y, 1);
+        this.speed = speed;
+        this.direction = direction;
+        this.isFrozen = true;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
+    public double getDx() {
+        return dx;
+    }
+
+    public double getDy() {
+        return dy;
+    }
+
+    public void setDirection(double direction) {
+        this.direction = direction;
+        double angle = Math.toRadians(direction);
+        dx = Math.cos(angle) * speed;
+        dy = -Math.sin(angle) * speed;
     }
 
     @Override
     public void draw(Canvas canvas) {
-
+        canvas.setPoint(x,y, 'O');
     }
 
     @Override
     public void move() {
+        if (!isFrozen){
+           x += dx;
+           y +=dy;
+        }
+    }
+
+    public void start() {
+        this.isFrozen = false;
+    }
+
+    void checkRebound(int minx, int maxx, int miny, int maxy){
 
     }
+
+
 }
