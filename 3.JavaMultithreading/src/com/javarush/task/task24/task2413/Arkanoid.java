bb829b95b2b899c7500226cc816bd2970dcf5ab8
Boris Osechinskiy <osechinskiy@gmail.com>
2022-08-15 11:16:28 +0300

    Task 2413.3

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -1,6 +1,30 @@
 package com.javarush.task.task24.task2413;
 
 public class Arkanoid {
+    private int width;
+    private int height;
+
+    public Arkanoid(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
     public static void main(String[] args) {
 
     }
