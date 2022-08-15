f36c880290b64cf36a723c85297370cf48a8ca42
Boris Osechinskiy <osechinskiy@gmail.com>
2022-08-15 11:36:40 +0300

    Task 2413.9

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/BaseObject.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/BaseObject.java
@@ -1,4 +1,49 @@
 package com.javarush.task.task24.task2413;
 
-public class BaseObject {
+public abstract class BaseObject {
+    private double x;
+    private double y;
+    private double radius;
+
+    public BaseObject(double x, double y, double radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public abstract void draw(Canvas canvas);
+
+    public abstract void move();
+
+    public boolean intersects(BaseObject o){
+        double dx = x - o.x;
+        double dy = y - o.y;
+        double destination = Math.sqrt(dx * dx + dy * dy);
+        double destination2 = Math.max(radius, o.radius);
+        return destination <= destination2;
+    }
 }
