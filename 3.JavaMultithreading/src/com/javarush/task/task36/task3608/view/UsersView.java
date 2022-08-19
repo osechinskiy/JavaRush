8aaef7f41e57b4939770c5b737e9bd3e31a7cc02
Boris Osechinskiy <osechinskiy@gmail.com>
2022-08-19 14:18:14 +0300

    MVC 6

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/UsersView.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/UsersView.java
@@ -4,14 +4,13 @@ import com.javarush.task.task36.task3608.bean.User;
 import com.javarush.task.task36.task3608.controller.Controller;
 import com.javarush.task.task36.task3608.model.ModelData;
 
-public class UsersView implements View{
+public class UsersView implements View {
+
     private Controller controller;
-
-
 
     @Override
     public void refresh(ModelData modelData) {
-        System.out.println("All users:");
+        System.out.println("All " + (modelData.isDisplayDeletedUserList() ? "deleted " : "") + "users:");
         for (User user : modelData.getUsers()) {
             System.out.println("\t" + user);
         }
@@ -22,6 +21,7 @@ public class UsersView implements View{
     public void setController(Controller controller) {
         this.controller = controller;
     }
+
     public void fireEventShowAllUsers() {
         controller.onShowAllUsers();
     }
